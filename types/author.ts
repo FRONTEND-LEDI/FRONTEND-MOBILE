fba8465c8aa1bd1523acc80771aa380493c73d28
tatymediina <tatimedinaiuni@gmail.com>
2025-10-20 12:08:28 -0300

    feat: :zap: Se cambió la vista de la biografía de los autores a la nueva estructura

--- types/author.ts
+++ types/author.ts
@@ -2,9 +2,13 @@ export type AuthorType = {
   _id: string;
   fullName: string;
   biography: string;
+  profession: string;
+  birthdate: Date;
+  birthplace: string;
+  nationality: string;
+  writingGenre: string[];
   avatar: {
     id_image: string;
     url_secura: string;
   };
-  bibliography?: string[];
-};
+}
